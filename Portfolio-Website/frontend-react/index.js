3f4665fa2ea8f3013b453b1f1481083f733f9053
Keanu <71466438+keanu-a@users.noreply.github.com>
2022-06-29 10:55:23 -0700

    adding base files

---
+++ Portfolio-Website/frontend-react/index.js
@@ -0,0 +1,11 @@
+// Keanu Aloua
+// June 12, 2022
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import "./index.css";
+
+// Connected app to the div in the html called root
+ReactDOM.render(<App />, document.getElementById("root"));
